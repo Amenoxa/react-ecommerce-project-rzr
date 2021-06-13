3aa2bfe54f5f4dd9f288321057888d2ef45af0ea
Amenoxa <48977442+Amenoxa@users.noreply.github.com>
2021-06-13 20:35:36 +0200

    sing in form
    +styles
    +components
    +button

---
+++ src/pages/loginpage/loginpage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/signin/signin.component'
+
+const LogInPage = () => (
+    <div className='login'>
+        <SignIn />
+    </div>
+);
+export default LogInPage;
