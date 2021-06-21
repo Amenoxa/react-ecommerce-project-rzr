f18a7735e7696de65560d298b553d2dedb355cb7
Amenoxa <48977442+Amenoxa@users.noreply.github.com>
2021-06-21 18:39:37 +0200

    sign up
    +component
    +styles
    +firebase auth and utils

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shoppage/shoppage.component';
 import LogInPage from './pages/loginpage/loginpage.component';
 
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    })
-  }
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth});
+       });
+    }
+  
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/pages/loginpage/loginpage.component.jsx
+++ src/pages/loginpage/loginpage.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import SignIn from '../../components/signin/signin.component'
+import SignIn from '../../components/signin/signin.component';
+import SignUp from '../../components/signup/sign-up.component';
+import './loginpage.styles.scss';
 
 const LogInPage = () => (
-    <div className='login'>
+    <div className='loginpage'>
         <SignIn />
+        <SignUp />
     </div>
 );
 export default LogInPage;
